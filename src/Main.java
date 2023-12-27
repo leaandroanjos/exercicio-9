f94e3b67abdbc31ab3bdf660e6b31ced82834ad4
Leandro <devleandrodosanjos@gmail.com>
2023-12-26 22:06:00 -0300

    Segundo commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int A = 2;
+
+        System.out.println(A);
+
+        Integer B = A;
+
+        System.out.println(B);
+
+    }
+}
